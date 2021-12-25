c27966f4c391b8086794180e401e235307dad136
Shoubhit Dash <shoubhit2005@gmail.com>
2021-12-25 18:10:36 +0530

    added a wish_me() function and README
    the wish_me() function greets the user according to the hour

---
+++ jarvis.py
@@ -0,0 +1,29 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voices", voices[0].id)
+
+# speak function
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# wishes the user
+def wish_me():
+    hour = int(datetime.datetime.now().hour)
+    # wishing according to the hour
+    if hour >= 5 and hour < 12:
+        speak("Good morning sir!")
+    
+    elif hour >= 12 and hour < 17:
+        speak("Good afternoon sir!")
+    
+    else:
+        speak("Good evening sir!")
+
+    speak("How may I help you?")
+    
+if __name__ == "__main__":
+    wish_me()                   # wishes the user first
