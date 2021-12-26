7b04971a191790f7e06a22d127efdc729cb0b2d0
Shoubhit Dash <shoubhit2005@gmail.com>
2021-12-26 20:50:50 +0530

    made a face detector using opencv
    it uses the haar cascade algorithm to detect the faces and draws a cool green rectangle around it

---
+++ Face_Detection/face_detector.py
@@ -0,0 +1,23 @@
+import cv2
+
+# loaded some pre-trained data on face frontals from opencv (haar cascade algorithm)
+trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# reading an image to detect faces in
+img = cv2.imread("RDJandTom.jpg")
+
+# converting the image to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detecting faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)       # coordinates of a rectangle
+
+# drawing rectangles around all the faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)       # (0, 255, 0), 2 => green colour for the rectangle with thickness of 2
+
+# showing the image
+cv2.imshow("Face Detector", img)
+cv2.waitKey()
+
+print("Code completed")
--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,8 @@
+'''
+Pausing this to first focus on the basics
+'''
+
+
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -6,15 +11,18 @@ engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
 engine.setProperty("voices", voices[0].id)
 
-# speak function
 def speak(audio):
+    '''
+    Enables Jarvis to speak
+    '''
     engine.say(audio)
     engine.runAndWait()
 
-# wishes the user
 def wish_me():
+    '''
+    Greets the user according to the hour
+    '''
     hour = int(datetime.datetime.now().hour)
-    # wishing according to the hour
     if hour >= 5 and hour < 12:
         speak("Good morning sir!")
     
@@ -26,5 +34,27 @@ def wish_me():
 
     speak("How may I help you?")
     
+def take_command():
+    '''
+    Takes micorphone input from the user and converts it into a string
+    '''
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print("Listening...")
+        r.pause_threshold = 1   # changing the pause threshold to 1 second
+        audio = r.listen(source)
+
+    try:
+        print("On it...")
+        query = r.recognize_google(audio, language="en-in")     # using the google speech recognition API
+        print("{query}\n")
+
+    except Exception as e:
+        print("Could you say that again, please...")            
+        return("None")
+
+    return(query)
+
 if __name__ == "__main__":
     wish_me()                   # wishes the user first
+    take_command()
